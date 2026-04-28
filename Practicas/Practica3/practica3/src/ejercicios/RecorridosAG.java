e4017aa03bbebcf4fe88e24cfbc04d942aaa5747
unknown <delfinaferrante1@gmail.com>
2026-04-28 20:29:15 -0300

    Termine ejercicio 2

--- Practicas/Practica3/practica3/src/ejercicios/RecorridosAG.java
+++ Practicas/Practica3/practica3/src/ejercicios/RecorridosAG.java
@@ -3,6 +3,7 @@ package ejercicios;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class RecorridosAG {
 
 	/* retorna una lista con los elementos impares del árbol “a” que sean mayores al valor “n”
@@ -15,19 +16,20 @@ public class RecorridosAG {
 		return resultado;
 	}
 	
-	private List<Integer> recorrerPreOrden(GeneralTree<Integer> arbol, Integer valor, List<Integer> lista) {
+	private void recorrerPreOrden(GeneralTree<Integer> arbol, Integer valor, List<Integer> lista) {
 		Integer dato = arbol.getData();
+		// Proceso raiz
 		if (dato > valor && dato % 2 != 0) {
 			lista.add(dato);
 		}
 		
+		// Luego los hijos
 		if (arbol.hasChildren()) {
 			List<GeneralTree<Integer>> hijos = arbol.getChildren();
 			for (GeneralTree<Integer> hijo: hijos) {
 				recorrerPreOrden(hijo, valor, lista);
 			}
 		}
-		return lista;
 	}
 	
 	//----------------------------------------------------------------
@@ -39,7 +41,7 @@ public class RecorridosAG {
 		return resultado;
 	}
 	
-	private List<Integer> recorrerInOrden(GeneralTree<Integer>arbol, Integer valor, List<Integer> lista){
+	private void recorrerInOrden(GeneralTree<Integer>arbol, Integer valor, List<Integer> lista){
 		List<GeneralTree<Integer>> hijos = arbol.getChildren();
 
 	    // Proceso PRIMER HIJO (si existe)
@@ -59,7 +61,60 @@ public class RecorridosAG {
 	            recorrerInOrden(hijos.get(i), valor, lista);
 	        }
 	    }
-	    return lista;
 	}
 	
+	//----------------------------------------------------------------
+	public List<Integer> numerosImparesMayoresQuePostOrden (GeneralTree <Integer> a, Integer n){
+		List<Integer> resultado = new LinkedList<Integer>();
+		if (a != null && !a.isEmpty()) {
+			recorrerPostOrden(a, n, resultado);
+		}
+		return resultado;
+	}
+	
+	private void recorrerPostOrden(GeneralTree<Integer> arbol, Integer valor, List<Integer> lista){
+		// Proceso los hijos
+		if (arbol.hasChildren()) {
+			List<GeneralTree<Integer>> hijos = new LinkedList<GeneralTree<Integer>>();
+			for (GeneralTree<Integer> hijo: hijos) {
+				recorrerPostOrden(hijo, valor, lista);
+			}
+		}
+		
+		// Proceso la raiz
+		Integer dato = arbol.getData();
+		if (dato > valor && dato % 2 != 0) {
+			lista.add(dato);
+		}
+	}
+
+	//----------------------------------------------------------------
+	public List<Integer> numerosImparesMayoresQuePorNiveles(GeneralTree <Integer> a, Integer n){
+		List<Integer> resultado = new LinkedList<Integer>();
+		if (a != null && !a.isEmpty()) {
+			recorrerPorNiveles(a, n, resultado);
+		}
+		return resultado;
+	}
+	
+	// nota: si me hubieran pedido contar cuantos valores que cumplen hay por nivel, tendria que haber puesto una marca "null" en la cola
+	private void recorrerPorNiveles(GeneralTree<Integer>arbol, Integer valor, List<Integer> lista) {
+		GeneralTree<Integer> arbolAux;
+		Queue<GeneralTree<Integer>> cola = new Queue<GeneralTree<Integer>>();
+		
+		cola.enqueue(arbol);
+		while (!cola.isEmpty()) {
+			arbolAux = cola.dequeue();
+			Integer dato = arbolAux.getData();
+			
+			if (dato > valor && dato % 2 != 0) {
+				lista.add(dato);
+			}
+			
+			List<GeneralTree<Integer>> hijos = arbolAux.getChildren();
+			for (GeneralTree<Integer> hijo: hijos) {
+				cola.enqueue(hijo);
+			}
+		}
+	}
 }
---
+++ Practicas/Practica3/practica3/src/ejercicios/Sequence.java
@@ -0,0 +1,8 @@
+package ejercicios;
+
+public abstract class Sequence {
+
+    public abstract int size();
+    public abstract boolean isEmpty();
+	
+}
